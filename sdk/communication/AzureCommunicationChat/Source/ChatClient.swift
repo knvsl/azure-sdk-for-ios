2d3ef3305a6824b137e5f52ea7d019e05e28aab2
knvsl <10719831+knvsl@users.noreply.github.com>
2021-05-26 10:38:19 -0700

    WIP debug tests

--- sdk/communication/AzureCommunicationChat/Source/ChatClient.swift
+++ sdk/communication/AzureCommunicationChat/Source/ChatClient.swift
@@ -147,6 +147,8 @@ public class ChatClient {
 
                 case let .failure(error):
                     completionHandler(.failure(error), httpResponse)
+                    let status = httpResponse?.statusCode
+                    print(status)
                 }
             }
         } catch {
--- sdk/communication/AzureCommunicationChat/Tests/ChatClientTests.swift
+++ sdk/communication/AzureCommunicationChat/Tests/ChatClientTests.swift
@@ -47,7 +47,13 @@ class ChatClientTests: XCTestCase {
         let endpoint = getEnvironmentVariable(withKey: "AZURE_COMMUNICATION_ENDPOINT", default: "https://endpoint")
         let token = generateToken()
         let credential = try CommunicationTokenCredential(token: token)
-        let options = AzureCommunicationChatClientOptions()
+        // let options = AzureCommunicationChatClientOptions()
+        let options = AzureCommunicationChatClientOptions(
+            logger: ClientLoggers.default(tag: "AzureCommunicationChatClient", level: .debug),
+            transportOptions: TransportOptions(
+                perRequestPolicies: [LoggingPolicy(allowHeaders: ["ms-cv", "Authorization"])]
+            )
+        )
 
         chatClient = try ChatClient(endpoint: endpoint, credential: credential, withOptions: options)
     }
